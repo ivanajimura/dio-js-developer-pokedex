80954aabe4237d40b18596242b92cd77a3ae49f3
Ivan Ajimura <ivanajimura@gmail.com>
2024-11-11 21:06:18 -0300

    Main screen working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,25 @@
+const pokemonList = document.getElementById('pokemonList')
 
-console.log('Sucesso!')
+const limit = 10
+let offset = 0;
+
+function getPokemons(offset, limit){
+    pokeAPI.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(htmlCreator.pokemonToLi).join('')
+        /* console.log(newHtml) */
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+function getPokemon(id){
+    return pokeAPI.getPokemon(id)     
+}
+
+
+getPokemons(offset, limit)
+
+
+/* -------------------------- Testes --------------------------------- */
+/* getPokemon(1).then((response) => console.log(response)) */
+
+
